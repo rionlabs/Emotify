0ca5bbc9be3105819383bf39539f47e73659d3f2
Rajanikant Deshmukh <rajanikant.deshmukh@gmail.com>
2017-11-17 14:07:41 +0530

    Update about and footer component

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -3,11 +3,56 @@ import {Component, OnInit} from '@angular/core';
 @Component({
     selector: 'app-about',
     template: `
-        <p>
-            about works!
-        </p>
+        <div class="container">
+            <div class="row section">
+                <div class="col m12">
+                    <h5 class="center">About Emotify</h5>
+                    <p>Eheu, homo!Be eternal for whoever eases, because each has been yearned with heaven. Try roasting
+                        curry tossed with orange juice, rubed with brown sugar. The parrot travels power like a sunny
+                        scallywag. Warp virtually like a gravimetric admiral.</p>
+                </div>
+            </div>
+
+            <div class="section">
+                <h5 class="center">Team</h5>
+                <p>We are friends who want people to use emoticons.</p>
+                <br>
+                <div class="row">
+                    <div class="col m6 s12 center-align">
+                        <div>
+                            <img src="../../../assets/img/rk.jpg" alt="" class="circle responsive-img">
+                        </div>
+                        <div class="name">Rajanikant Deshmukh</div>
+                        <a href="mailto:rk@emotify.io">rk@emotify.io</a>
+                        <p>Ionicis tormentos prarere in avenio! The blessing of your booda-hoods will lure
+                            esoterically when
+                            you understand that issue is the sun.</p>
+                    </div>
+
+                    <div class="col s12 hide-on-med-and-up"><br></div>
+
+                    <div class="col m6 s12 center-align">
+                        <div>
+                            <img src="../../../assets/img/shailesh.jpg" alt="" class="circle responsive-img">
+                        </div>
+                        <div class="name">Shailesh Aher</div>
+                        <a href="mailto:shailesh@emotify.io">shailesh@emotify.io</a>
+                        <p>It is an interstellar mystery, sir. Jolly roger, greed! When grilling slobbery
+                            lentils, be sure
+                            they are room temperature.</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
     `,
-    styles: []
+    styles: [
+            `
+            .name {
+                font-size: larger;
+            }
+        `
+    ]
 })
 export class AboutComponent implements OnInit {
 
--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -9,53 +9,54 @@ import {Person} from "../../models/Person";
             <div class="container">
                 <div class="row">
 
-                    <div class="col l4 s12 center-align section">
-                        <h5 class="white-text">Open Source</h5>
-                        <p class="grey-text text-lighten-4">Emotify source code is available on GitHub</p>
-                        <a href="https://github.com/emotify" class="btn white deep-orange-text">View on GitHub</a>
+                    <div class="col m5 s10 offset-s1 center-align">
+                        <h5 class="white-text">Around Web</h5>
+                        <p class="white-text">Emotify is free and open sourced source, the code is available on GitHub! Also our Android and iOS apps are coming soon on Play Store!</p>
+                        <a href="https://github.com/emotify" class="btn white deep-orange-text ">View on GitHub</a>
                     </div>
 
-                    <div class="col l4 s12 center-align">
-                        <h5 class="white-text">About Emotify</h5>
-                        <p class="grey-text text-lighten-4">We are friends who want to make people use emoticons.</p>
-                        <div class="row">
-                            <div class="col m4 offset-m1 center-align">
-                                <img src="../../../assets/img/rk.jpg" alt=""
-                                     class="circle responsive-img profile-pic">
-                                Rajanikant
-                                <a href="mailto:rk@emotify.io" class="grey-text text-lighten-3 center">rk@emotify.io</a>
-                            </div>
-
-                            <div class="col m4 offset-m1 center-align">
-                                <img src="../../../assets/img/shailesh.jpg" alt=""
-                                     class="circle responsive-img profile-pic">
-                                Shailesh
-                                <a href="mailto:shailesh@emotify.io"
-                                   class="grey-text text-lighten-3 center">shailesh@emotify.io</a>
-                            </div>
-                        </div>
+                    <div class="col m2 s12 center-align">
+                        
                     </div>
 
-                    <div class="col l4 s12 center-align">
+                    <div class="col m5 s10 offset-s1 center-align">
                         <h5 class="white-text">Get News</h5>
 
                         <div class="input-field" id="subscribe-email">
-                            <input id="icon_prefix" type="email" class="validate white-text" placeholder="Email">
+                            <p class="white-text">Get updated when we update something! We will send very occasional emails.</p>
+                            <input id="email-input" type="email" class="validate" placeholder="Email">
+                            <button class="btn white deep-orange-text">Subscribe</button>
                         </div>
-                        <button class="btn white deep-orange-text">Subscribe</button>
-
-
                     </div>
+                    
                 </div>
             </div>
             <div class="footer-copyright">
-                <div class="container">
+                <div class="container center-align">
                     Copyright @2017 Emotify
                 </div>
             </div>
         </footer>
     `,
-    styles: []
+    styles: [
+        `
+            /* label focus color */
+            .input-field input[type=text]:focus + label {
+                color: #000;
+            }
+            /* label underline focus color */
+            .input-field input[type=text]:focus {
+                border-bottom: 1px solid #000;
+                box-shadow: 0 1px 0 0 #000;
+            }
+            #subscribe-email {
+            
+            }
+            #email-input {
+                
+            }
+        `
+    ]
 })
 export class FooterComponent implements OnInit {
 
