7a6f512e7d6b77879efee69bdbf4a1507c268c53
Rajanikant Deshmukh <rajanikant.deshmukh@gmail.com>
2017-11-24 18:55:22 +0530

    Update static contents and about page

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -8,43 +8,81 @@ import {Contents} from "../../static-contents";
             <div class="row section">
                 <div class="col m12">
                     <h5 class="center">About Emotify</h5>
-                    <p>{{about_emotify}}</p>
+                    <p>{{about_emotify[0]}}</p>
+                    <p>{{about_emotify[1]}}</p>
                 </div>
             </div>
 
             <div class="section">
                 <h5 class="center">Team</h5>
-                <p>{{about_team}}</p>
                 <br>
                 <div class="row">
-                    <div class="col m6 s12 center-align">
+                    <div class="col offset-l2 l4 offset-m1 m5 s12 center-align">
                         <div>
-                            <img src="../../../assets/img/rk.jpg" alt="" class="circle responsive-img">
+                            <img src="../../../assets/img/rk.jpg" alt="" class="profile-img  circle responsive-img">
                         </div>
+                        <br>
                         <div class="name">{{team.rk.name}}</div>
                         <a href="mailto:{{team.rk.email}}">{{team.rk.email}}</a>
-                        <p>{{team.rk.about}}</p>
+                
+                        <div class="social-container">
+                            <a target="_blank" href="{{team.rk.github_url}}">
+                                <img class="social-icon" src="../../../assets/svg/github.svg">
+                            </a>
+                            <a target="_blank" href="{{team.rk.twitter_url}}">
+                                <img class="social-icon" src="../../../assets/svg/twitter.svg">
+                            </a>
+                            <a target="_blank" href="{{team.rk.linkedin_url}}">
+                                <img class="social-icon" src="../../../assets/svg/linkedin.svg">
+                            </a>
+                        </div>
+
                     </div>
 
                     <div class="col s12 hide-on-med-and-up"><br></div>
 
-                    <div class="col m6 s12 center-align">
+                    <div class="col l4 m5 s12 center-align">
                         <div>
-                            <img src="../../../assets/img/shailesh.jpg" alt="" class="circle responsive-img">
+                            <img src="../../../assets/img/shaily.png" alt="" class="profile-img circle responsive-img">
                         </div>
+                        <br>
                         <div class="name">{{team.shaily.name}}</div>
                         <a href="mailto:{{team.shaily.email}}">{{team.shaily.email}}</a>
-                        <p>{{team.shaily.about}}</p>
+                        <div class="social-container">
+                            <a target="_blank" href="{{team.shaily.github_url}}">
+                                <img class="social-icon" src="../../../assets/svg/github.svg">
+                            </a>
+                            <a target="_blank" href="{{team.shaily.twitter_url}}">
+                                <img class="social-icon" src="../../../assets/svg/twitter.svg">
+                            </a>
+                            <a target="_blank" href="{{team.shaily.linkedin_url}}">
+                                <img class="social-icon" src="../../../assets/svg/linkedin.svg">
+                            </a>
+                        </div>
                     </div>
-
                 </div>
             </div>
+
         </div>
     `,
     styles: [
             `
+            .profile-img {
+                width: 150px;
+                height: auto;
+            }
             .name {
                 font-size: larger;
+            }
+
+            .social-container {
+                margin: 1rem 0;
+            }
+
+            .social-icon {
+                margin: 0 8px;
+                width: 32px;
+                height: 32px;
             }
         `
     ]
@@ -53,7 +91,6 @@ export class AboutComponent implements OnInit {
 
     team = Contents.team;
     about_emotify = Contents.about_emotify_text;
-    about_team = Contents.about_team_text;
 
     constructor() {
     }
--- src/app/static-contents.ts
+++ src/app/static-contents.ts
@@ -5,24 +5,33 @@ export const Contents = {
     banner_title: "Use Emoticons",
     banner_text: "Emoticons are emojis created with ASCII characters",
 
-    contact_email : 'mail@emotify.io',
+    contact_email: 'mail@emotify.io',
     github_issue_url: "https://github.com/emotify/web/issues",
     github_url: "https://github.com/emotify",
     subscribe_form_action_url: "TODO",
 
-    about_emotify_text: "Eheu, homo!Be eternal for whoever eases, because each has been yearned with heaven. Try roasting curry tossed with orange juice, rubed with brown sugar. The parrot travels power like a sunny scallywag. Warp virtually like a gravimetric admiral.",
+    about_emotify_text: [
+        "First things first! Emojis and Emoticon are not the same things. You can emoticons are the great-grandfather of emojis, what we use nowadays.This was a classic way of communicating and was a huge thing when the internet first came out. Emoticons are also known as Kaomoji (顔文字), of course of Japanese origin.",
+        "We here at emotift.io want to bring a collection of emoticons to people who still want to use them and make it available to everyone for free. Please support us! If you are having any troubles or have suggestions feel free to contact us!"
+    ],
     about_team_text: "We are friends who want people to use emoticons.",
 
     team: {
         rk: {
             "about": "Ionicis tormentos prarere in avenio! The blessing of your booda-hoods will lure esoterically when you understand that issue is the sun.",
             "email": "rk@emotify.io",
-            "name": "Rajanikant Deshmukh"
+            "name": "Rajanikant Deshmukh",
+            "github_url": "https://github.com/rajanikantdeshmukh",
+            "twitter_url": "https://twitter.com/_alpharion_",
+            "linkedin_url": "https://www.linkedin.com/in/rajanikantdeshmukh/"
         },
         shaily: {
             "about": "It is an interstellar mystery, sir. Jolly roger, greed! When grilling slobbery lentils, be sure they are room temperature.",
             "email": "shailesh@emotify.io",
-            "name": "Shailesh Aher"
+            "name": "Shailesh Aher",
+            "github_url": "https://github.com/ahershailesh",
+            "twitter_url": "https://twitter.com/https://twitter.com/AherShailu",
+            "linkedin_url": "https://www.linkedin.com/in/shailesh-aher-44a0b096"
         }
     }
 };
