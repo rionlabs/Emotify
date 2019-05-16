ef4b08e4ce614addb9a11e83c4fdb671f4db475a
Rajanikant Deshmukh <rajanikant.deshmukh@gmail.com>
2019-05-17 07:53:54 +0900

    Remove clutter from footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -3,73 +3,16 @@ import {Component, OnInit} from '@angular/core';
 @Component({
     selector: 'app-footer',
     template: `
-        <footer class="page-footer deep-orange">
-            <div class="container">
-                <div class="row">
-
-                    <div class="col m5 s10 offset-s1 center-align">
-                        <h5 class="white-text">Around Web</h5>
-                        <p class="white-text">Emotify is free and open sourced source, the code is available on GitHub!
-                            Also our Android and iOS apps are coming soon on Play Store!</p>
-                        <a href="https://github.com/rionlabs/emotify-web-app" class="btn white deep-orange-text ">View
-                            on GitHub</a>
-                    </div>
-
-                    <div class="col m2 s12 center-align">
-
-                    </div>
-
-                    <div class="col m5 s10 offset-s1 center-align">
-                        <h5 class="white-text">Get News</h5>
-
-                        <div class="input-field" id="subscribe-email">
-                            <p class="white-text">Get updated when we update something! We will send very occasional
-                                emails.</p>
-                            <input id="email-input" type="email" class="validate" placeholder="Email">
-                            <button class="btn white deep-orange-text">Subscribe</button>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-            <div class="footer-copyright">
+        <footer>
+            <mat-toolbar>
                 <div class="container center-align">
-                    <span id="rionlabs">rionlabs</span>　&　ヽ(♡‿♡)ノ
+                    <span id="rionlabs-logo">rionlabs</span>　&　ヽ(♡‿♡)ノ
                 </div>
-            </div>
+            </mat-toolbar>
         </footer>
-    `,
-    styles: [
-            `
-            /* label focus color */
-            .input-field input[type=text]:focus + label {
-                color: #000;
-            }
-
-            /* label underline focus color */
-            .input-field input[type=text]:focus {
-                border-bottom: 1px solid #000;
-                box-shadow: 0 1px 0 0 #000;
-            }
-
-            #subscribe-email {
-
-            }
-
-            #email-input {
-
-            }
-
-            #rionlabs {
-                font-family: 'Orbitron', sans-serif;
-            }
-        `
-    ]
+    `
 })
 export class FooterComponent implements OnInit {
-
-    constructor() {
-    }
 
     ngOnInit() {
     }
--- src/app/static-contents.ts
+++
@@ -1,5 +0,0 @@
-export const Contents = {
-    contact_email: 'mail@emotify.io',
-    github_issue_url: "https://github.com/rionlabs/emotify-web-app/issues",
-    github_url: "https://github.com/rionlabs/emotify-web-app"
-};
