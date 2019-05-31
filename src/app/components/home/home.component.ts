5787bb3fe4e67a023b74d40a16a72313b29f6b0a
Rajanikant Deshmukh <rajanikant.deshmukh@quipper.com>
2019-05-31 20:10:46 +0900

    Update emoticon component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,9 +3,29 @@ import {Component, OnInit} from '@angular/core';
 @Component({
     selector: 'app-home',
     template: `
-        <app-emoticons></app-emoticons>
+        <div class="container">
+            <app-emoticons></app-emoticons>
+        </div>
     `,
-    styles: []
+    styles: [`
+        .container {
+            margin: 0 auto;
+            max-width: 1280px;
+            width: 95%
+        }
+
+        @media only screen and (min-width: 601px) {
+            .container {
+                width: 90%
+            }
+        }
+
+        @media only screen and (min-width: 993px) {
+            .container {
+                width: 85%
+            }
+        }
+    `]
 })
 export class HomeComponent implements OnInit {
 
